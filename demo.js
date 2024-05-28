716f9c3452f79abb0b920ed5979751042615b90f
Himu946 <himanshumalik946@gmail.com>
2024-05-28 23:54:10 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,5 @@
+const subHeading=document.createElement('h3');
+const text= document.createTextNode('Buy high quality organic fruits online');
+subHeading.appendChild(text);
+subHeading.style.fontWeight="italic";
+
